7ee01eebe4e4b9bc5527925ee80d5f73b4ccca56
npcantrell <70540978+npcantrell@users.noreply.github.com>
2021-03-03 13:11:18 -0700

    Update toLatex.py

--- toLatex.py
+++ toLatex.py
@@ -56,7 +56,7 @@ if len(sys.argv) == 4:
 outFile.write("\\begin{tcolorbox}[width=\\linewidth, breakable]" + "\n")
 
 for line in inFile:
-    writeLine = line.replace("{", "\\{").replace("}", "\\}").replace("\\", "{\\textbackslash}").replace(" ", "\\ ")
+    writeLine = line.replace("\\", "{\\textbackslash}").replace("{", "\\{").replace("}", "\\}").replace(" ", "\\ ")
     writeLine = writeLine.replace("\n", "").replace("^", "\\^").replace("&", "\\&").replace("$", "\\$").replace("_", "\\_")
     writeLine = writeLine.replace("#", "\\#").replace("%", "\\%").replace("\t", "\\ \\ \\ \\ ")
 
